2aa17be669c6563635e35a0beaeb0116beaf0152
Max Jeon <maxjeon97@gmail.com>
2024-05-02 11:01:59 -0700

    add middleware to work with clerk

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,9 @@
-export default function middleware(){
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-}
+// const isProtectedRoute = createRouteMatcher([]);
+
+export default clerkMiddleware();
+
+export const config = {
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+};
