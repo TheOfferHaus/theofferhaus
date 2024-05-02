e9059126dd21835be2854f7c30309f4fd5d061a4
Samuel Myung <shmyung4@gmail.com>
2024-05-02 09:59:42 -0700

    Added the PaymentForm component as well as downloaded the necessary dependencies.

---
+++ app/components/stripe_checkout_form/page.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import React from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+import {
+  EmbeddedCheckoutProvider,
+  EmbeddedCheckout
+} from '@stripe/react-stripe-js';
+
+// Configure the Stripe library
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
+
+/** Rendering the pre-built Stripe checkout/payment form
+ *
+ * State: None
+ *
+ * Props: clientSecret, a string that works as a key to a unique Checkout Session
+ *
+ * Payment -> PaymentForm
+*/
+export default function PaymentForm({clientSecret}: {clientSecret: string}) {
+  const options = {clientSecret}
+return (
+    <div id="checkout">
+      <EmbeddedCheckoutProvider
+        stripe={stripePromise}
+        options={options}
+      >
+        <EmbeddedCheckout />
+      </EmbeddedCheckoutProvider>
+    </div>
+  );
+}
