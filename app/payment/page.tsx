43af062833062bddd1b8ae6b95d8975eaaee8478
David Sapiro <dsap48@gmail.com>
2024-05-02 16:57:16 -0700

    Updated payment page to dynamically include user data
    Get current user, check in db if they have a stripe id, if not, create one and update the db

--- app/payment/page.tsx
+++ app/payment/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import PaymentForm from '../../components/PaymentForm';
-import Stripe from 'stripe';
 import StripeApi from '../utils/stripeApi';
-import Footer from '@/components/Footer';
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+import { currentUser } from '@clerk/nextjs/server';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient({
+    datasourceUrl: process.env.DATABASE_URL,
+});
 
 /** Creates a Customer object and Checkout Session object for the user, providing
  * a client secret to the PaymentForm component
@@ -15,17 +17,32 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
  * Payments -> PaymentForm
 */
 export default async function Payment() {
-    let customerId = "cus_Q28jd2XLv2Sfwy"; //will query from db once available
-    let priceId = "pr_1234";
+    const user = await currentUser();
+    const email = user?.emailAddresses[0].emailAddress;
+    const name = user?.fullName;
+    const username = user?.username!;
 
-    if (customerId === null) {
-        const customerId= await StripeApi.createCustomer()
+    const userData = await prisma.user.findUnique({
+        where: {
+            username: username,
+        },
+    });
 
-        //update database with customer.id
+    const priceId = "price_1PC4LDRrTWD9lwhqkYBSUPmV"; //will be received from a url param when called
+
+    let stripeCustomerId: string | null = userData!.stripeId;
+
+    if (stripeCustomerId === null && user) {
+        stripeCustomerId = await StripeApi.createCustomer(name as string, email as string);
+
+        const user = await prisma.user.update({
+            where: { username: username },
+            data: { stripeId: stripeCustomerId },
+        });
     }
-
+    console.log("customerId after customer creation:", stripeCustomerId);
     // Create Checkout Sessions from body params.
-    const session = await StripeApi.createCheckoutSession(customerId, priceId);
+    const session = await StripeApi.createCheckoutSession(stripeCustomerId!, priceId);
 
     return (
         <div id="checkout">
--- app/paymentConfirmation/page.tsx
+++ app/paymentConfirmation/page.tsx
@@ -19,15 +19,16 @@ export default async function Return(data: { searchParams: { session_id: string;
     const sessionId = data.searchParams.session_id;
     const sessionData = await StripeApi.retrieveCheckoutSession(sessionId);
 
-        return (
-            sessionData!.status === 'complete'
-                ? <section id="success">
-                    <p>
-                        We appreciate your business! A confirmation email will be sent to {sessionData!.customer_details!.email}.
+    // returns generic success message. Need to update.
+    return (
+        sessionData!.status === 'complete'
+            ? <section id="success">
+                <p>
+                    We appreciate your business! A confirmation email will be sent to {sessionData!.customer_details!.email}.
 
-                        If you have any questions, please email <a href="mailto:orders@example.com">orders@example.com</a>.
-                    </p>
-                </section>
-                : redirect('/')
-        );
-    }
+                    If you have any questions, please email <a href="mailto:orders@example.com">orders@example.com</a>.
+                </p>
+            </section>
+            : redirect('/')
+    );
+}
