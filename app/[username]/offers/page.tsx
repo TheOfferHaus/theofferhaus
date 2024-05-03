72db0d97660067a00353ac53dfa2370a018ebf46
Max Jeon <maxjeon97@gmail.com>
2024-05-02 17:33:07 -0700

    add column to offers schema and make skeleton for /[username]/offers page

--- app/[username]/offers/page.tsx
+++ app/[username]/offers/page.tsx
@@ -1,36 +1,60 @@
 import { currentUser } from "@clerk/nextjs/server";
+import { PrismaClient } from "@prisma/client";
 import { redirect } from "next/navigation";
 
-/** List of all the offer documents user has generated with our platform */
+const prisma = new PrismaClient();
 
-export default async function Offers({ params } : {params: {username: string}}) {
 
-    const  { username } = params;
+/** Lists all the offer documents user has generated with our platform. Includes
+ * offer ID and link to corresponding Docusign envelope
+ *
+ * Ensures that users can only access their own offers
+ */
 
+export default async function Offers({ params }: { params: { username: string; }; }) {
+    const { username } = params;
     const currUser = await currentUser();
+
     if (username !== currUser!.username) redirect(`/${currUser!.username}/offers`);
-    console.log(currUser);
 
-    /**
-     *
-     *
-     * when offer is made an evelope id is generated
-     * offer and envelope id is put into the database
-     *
-     * when getting to this page we:
-     * query for user's offers
-     * use envelope id from each offer to fetch from docusign API for link to specific envelope
-     * map over links from API call to make li
-     *
-     * offer id: #12
-     *
-     * link leads to docusign ---> sign-in
-     *
-     *
-     *
-    */
+    const user = await prisma.user.findUnique({
+        where: {
+            username: username
+        },
+        include: {
+            offers: {
+                include: {
+                    property: true
+                },
+                orderBy: {
+                    id: 'asc',
+                }
+            }
+        }
+    });
+
+    const offerIds = user?.offers.map(o => ({
+        id: o.id,
+        envelopeId: o.envelopeId,
+    }));
+
+    // this array will be passed into a function that gives us back an array of
+    // objects with each object having the envelopeId and the corresponding link
+    // to the docusign envelope
 
 
+    const offers = [{ id: "id", envelopeId: "string", docusignLink: "link" }, {}];
+
+
+    // return statement would map through the offers array that now contains
+    // both offerId AND a link to the envelope, and would render a Offer display
+    // component for each one
+
+    return (
+        <div>
+            {offers.map(o => <OfferCard key={o.id} offer={o} />)}
+        </div>
+    )
 
 
     return (
