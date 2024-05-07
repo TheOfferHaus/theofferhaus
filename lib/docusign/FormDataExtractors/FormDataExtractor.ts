4c07d35071d672a4226667c09ec1a8b6606397b6
Cooper Spinelli <cooperspinelli@gmail.com>
2024-05-07 15:01:40 -0600

    Write classes for formatting form data for docusign

---
+++ lib/docusign/FormDataExtractors/FormDataExtractor.ts
@@ -0,0 +1,77 @@
+type ExtractedAnswers = {
+  [key: string]: string | boolean;
+};
+
+type FormattedExtractedAnswers = {
+  [key: string]: string;
+};
+
+
+interface Answer {
+  field: { ref: string; };
+  type: string;
+  choice?: { label: string; };
+  boolean?: boolean;
+  text?: string;
+  number?: number;
+  date?: string;
+}
+
+const dataExtractors: { [key: string]: (input: Answer) => any; } = {
+  choice: (ans: Answer) => ans.choice?.label,
+  boolean: (ans: Answer) => ans.boolean,
+  text: (ans: Answer) => ans.text,
+  number: (ans: Answer) => ans.number,
+  date: (ans: Answer) => ans.date,
+};
+
+const generalInfo = [
+  'buyer_email',
+  'buyers_name',
+  'current_buyer_address',
+  'sellers_name',
+  'sellers_current_address',
+  'sellers_current_city',
+  'sellers_current_state',
+  'home_type',
+  'offer_amount_num',
+  'offer_made_on_date',
+  'verify_sufficient_funds_deadline_date',
+  'verify_sufficient_funds_deadline_time',
+  'verify_sufficient_funds_seller_days_num',
+  'closing_time_deadline',
+];
+
+export default class FormDataExtractor {
+  extractedAnswers: ExtractedAnswers;
+
+  additionalInfoExtractors: {
+    [key: string]: (input: ExtractedAnswers) => string;
+  } = {};
+
+  constructor(formData: any[]) {
+    this.extractedAnswers = this.extractAnswers(formData);
+  }
+
+  extractAnswers(answers: Answer[]): ExtractedAnswers {
+    return Object.fromEntries(
+      answers.map(answer =>
+        [answer.field.ref, dataExtractors[answer.type](answer)]));
+  }
+
+  formatGeneralInfoAnswersForDocusign(answers: ExtractedAnswers): FormattedExtractedAnswers {
+    return Object.fromEntries(
+      generalInfo.map(field => [field, String(answers[field])])
+    );
+  }
+
+  formatAnswersForDocusign(answers: ExtractedAnswers): { [key: string]: string; } {
+    const formatted = this.formatGeneralInfoAnswersForDocusign(answers);
+
+    for (const [field, extractor] of Object.entries(this.additionalInfoExtractors)) {
+      formatted[field] = extractor(answers);
+    }
+
+    return formatted;
+  }
+}
