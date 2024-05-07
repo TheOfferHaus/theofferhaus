ebd515528590c1e8b78846d4fa93fcb584fbb0dc
Gracee Gallivan <gracee.gallivan@gmail.com>
2024-05-07 11:36:07 -0700

    added new global color scheme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,18 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      "primary-dark": "#001BCC",
+      "primary-medium": "#007FFF",
+      "off-white": "#F2F1EB",
+      "custom-white": "#fffdf7",
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      // backgroundImage: {
+      //   "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      //   "gradient-conic":
+      //     "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      // },
     },
   },
   plugins: [],
