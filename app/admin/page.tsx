e00fff0867282dc3748012b698a4339939be2c68
David Sapiro <dsap48@gmail.com>
2024-05-07 09:55:15 -0700

    Added logic to pull all users from Clerk in the admin page component

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,16 @@
-export default function AdminDashboard() {
+import { clerkClient } from "@clerk/nextjs/server";
+// leaving for when we need to query the db
+// import { PrismaClient } from '@prisma/client';
+// const prisma = new PrismaClient();
+
+
+export default async function AdminDashboard() {
+
+    const users = await clerkClient.users.getUserList({
+        orderBy: "-username"
+    });
+
     return (
-        <h1>Cannot see this if not logged in!</h1>
+        <div>Admins only!</div>
     );
 }
