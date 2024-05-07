e6090d025c568f2cf029a06cf3bb770ca6ff98f6
Max Jeon <maxjeon97@gmail.com>
2024-05-07 15:51:52 -0700

    Build out basic routing for how we will go to quizzes that are specific to a propertyId

--- app/quiz/page.tsx
+++ app/quiz/page.tsx
@@ -1,14 +1,19 @@
 import FormWidget from "@/components/TypeformWidget";
+import { redirect } from "next/navigation";
 
 /** Quiz:  Component for rendering the Quiz page.
  *
  * Quiz -> FormWidget
  */
 
-function Quiz() {
+function Quiz({ searchParams }: { searchParams: { propertyId: string | null; }; }) {
+  const propertyId = searchParams.propertyId;
+
+  if (!propertyId) redirect("/");
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <FormWidget />
+      <FormWidget propertyId={propertyId} />
     </main>
   );
 }
--- components/TypeformWidget.tsx
+++ components/TypeformWidget.tsx
@@ -5,16 +5,8 @@ import { TYPEFORM_FORM_ID } from '@/constants';
 
 /**Component for rendering a TypeForm widget (embeds the form) */
 
-const FormWidget = () => {
-
-    // function handleFinalQuestion({formId, ref} : {formId: string, ref: string}) {
-    //     // check if user is logged in via clerk
-    //     // if not logged in redirect to signin/signup page, which will redirect back to current form and question
-    // }
+export default function FormWidget({ propertyId } : { propertyId: string}) {
     return <Widget id={TYPEFORM_FORM_ID}
                    className="fixed inset-0 overflow-auto bg-white"
-                   keepSession={true} />
-                //    onQuestionChanged={handleFinalQuestion}/>;
+                   hidden={{ property_id: propertyId}} />
 };
-
-export default FormWidget;
