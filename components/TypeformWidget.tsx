f83d0516d5efd15fc34676f202e57eb08a79a95e
Ben Briones <benbriones50@gmail.com>
2024-05-06 17:16:01 -0700

    researching typeform API to redirect to end if user is verified

--- components/TypeformWidget.tsx
+++ components/TypeformWidget.tsx
@@ -6,7 +6,15 @@ import { TYPEFORM_FORM_ID } from '@/constants';
 /**Component for rendering a TypeForm widget (embeds the form) */
 
 const FormWidget = () => {
-    return <Widget id={TYPEFORM_FORM_ID} className="fixed inset-0 overflow-auto bg-white" />;
+
+    // function handleFinalQuestion({formId, ref} : {formId: string, ref: string}) {
+    //     // check if user is logged in via clerk
+    //     // if not logged in redirect to signin/signup page, which will redirect back to current form and question
+    // }
+    return <Widget id={TYPEFORM_FORM_ID}
+                   className="fixed inset-0 overflow-auto bg-white"
+                   keepSession={true} />
+                //    onQuestionChanged={handleFinalQuestion}/>;
 };
 
 export default FormWidget;
