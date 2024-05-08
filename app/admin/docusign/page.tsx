c9dd93125d48d3defe05fd30f9b61f455b19b282
Cooper Spinelli <cooperspinelli@gmail.com>
2024-05-08 15:55:29 -0600

    Remove code search param after getting access code

--- app/admin/docusign/page.tsx
+++ app/admin/docusign/page.tsx
@@ -6,80 +6,73 @@ import RESIDENTIAL_PURCHASE_AGREEMENT_DUMMY_DATA from "@/lib/docusign/agreementD
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { FormEvent } from "react";
-import { useSearchParams } from "next/navigation";
+import { useSearchParams, useRouter } from "next/navigation";
 
 const DOCUSIGN_CODE_URL = `https://account-d.docusign.com/password?response_type=code&scope=signature%20impersonation&client_id=${process.env.NEXT_PUBLIC_DOCUSIGN_INTEGRATION_KEY}&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fadmin%2Fdocusign`;
 
 export default function DocusignPage() {
-    const params = useSearchParams();
-    const [documentUrl, setDocumentUrl] = useState("");
-    const [templateId, setTemplateId] = useState("");
-    const { user } = useUser();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const [documentUrl, setDocumentUrl] = useState("");
+  const [templateId, setTemplateId] = useState("");
+  const { user } = useUser();
 
-    useEffect(function createTokenOnMount() {
-        async function createToken() {
-            generateAccessDataAfterConsent(params.get('code')!);
-        }
+  useEffect(function createTokenOnMount() {
+    async function createToken() {
+      await generateAccessDataAfterConsent(searchParams.get('code')!);
 
-        if (params.get('code')) {
-            createToken();
-        }
-    }, []);
-
-    async function handleSendEnvelope(evt: FormEvent) {
-        evt.preventDefault();
-
-        const signerData = {
-            name: `${user?.firstName} ${user?.lastName}`,
-            email: user?.emailAddresses[0].emailAddress!
-        };
-
-        const envelopeId = await makeEnvelope(RESIDENTIAL_PURCHASE_AGREEMENT_DUMMY_DATA, signerData);
-        await sendEnvelopeEmail(envelopeId);
-        const envelopeUrl = await getEnvelopeUrl(envelopeId, signerData);
-        setDocumentUrl(envelopeUrl);
+      // Remove query param (if they refresh with the code param then it
+      // will throw an error if the authorization code has expired)
+      router.push("http://localhost:3000/admin/docusign");
     }
 
-    async function handleCreateTemplate(evt: FormEvent) {
-        evt.preventDefault();
-        const newTemplateId = await createTemplate();
-        setTemplateId(newTemplateId);
+    if (searchParams.get('code')) {
+      createToken();
     }
+  }, []);
 
-    async function handleGetEnvelopes(evt: FormEvent) {
-        evt.preventDefault();
-        const signerData = {
-            name: `${user?.firstName} ${user?.lastName}`,
-            email: user?.emailAddresses[0].emailAddress!
-        };
-    }
 
-    if (!user) return <h1>Loading</h1>;
+  async function handleSendEnvelope(evt: FormEvent) {
+    evt.preventDefault();
 
-    return (
-        <div>
-            <div>
-                <button onClick={handleSendEnvelope}>Make and Send Envelope</button>
-            </div>
-            <div>
-                <button onClick={handleCreateTemplate}>Create Template</button>
-            </div>
-            <div>
-                <button onClick={handleGetEnvelopes}>Get Envelopes</button>
-            </div>
-            <div>
-                <a href={DOCUSIGN_CODE_URL}>Get token</a>
-            </div>
-            <div>
-                {documentUrl && <Link href={documentUrl}>Sign document</Link>}
-            </div>
-            <div>
-                {templateId}
-            </div>
-            <div>
-                {user?.emailAddresses[0].emailAddress!}
-            </div>
-        </div>
-    );
+    const signerData = {
+      name: `${user?.firstName} ${user?.lastName}`,
+      email: user?.emailAddresses[0].emailAddress!
+    };
+
+    const envelopeId = await makeEnvelope(RESIDENTIAL_PURCHASE_AGREEMENT_DUMMY_DATA, signerData);
+    await sendEnvelopeEmail(envelopeId);
+    const envelopeUrl = await getEnvelopeUrl(envelopeId, signerData);
+    setDocumentUrl(envelopeUrl);
+  }
+
+
+  async function handleCreateTemplate(evt: FormEvent) {
+    evt.preventDefault();
+    const newTemplateId = await createTemplate();
+    setTemplateId(newTemplateId);
+  }
+
+  if (!user) return <h1>Loading</h1>;
+
+  return (
+    <div>
+      <div>
+        <button onClick={handleSendEnvelope}>Make and Send Envelope</button>
+      </div>
+      <div>
+        <button onClick={handleCreateTemplate}>Create Template</button>
+      </div>
+      <div>
+        <Link href={DOCUSIGN_CODE_URL}>Get token</Link>
+      </div>
+      <div>
+        {documentUrl && <Link href={documentUrl}>Sign document</Link>}
+      </div>
+      <div>
+        {templateId}
+      </div>
+    </div>
+  );
 }
 
