c8067e81ae7aed0b3f996b40bbcd3f2addc95489
Jazz Cheema <jaskamalc@gmail.com>
2024-05-07 20:24:56 -0700

    created a ClerkUser component to be used in NavBar - in addition: removed comments in NavBar component

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,18 +1,5 @@
-import { SignedIn, SignedOut, UserButton, SignInButton } from "@clerk/nextjs";
 import Link from "next/link";
-
-const ClerkUserMenuItem = () => {
-  return (
-    <div>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </div>
-  );
-};
+import ClerkUserMenuItem from "./ClerkUserMenuItem";
 
 /** NavBar component */
 
@@ -20,25 +7,17 @@ const NavBar = () => {
   return (
     <nav className="absolute w-full bg-custom-white z-50">
       <div className="flex px-4 sm:px-6 lg:px-8 justify-between h-16 mx-auto">
-            {/* Navigation links */}
-            <div className="flex items-center space-x-8 font-semibold">
-              <Link
-                className="px-3 py-2 hover:text-primary-dark"
-                href="/"
-              >
-                Home
-              </Link>
-              <Link
-                className="px-3 py-2 hover:text-primary-dark"
-                href="/quiz"
-              >
-                Quiz
-              </Link>
-          </div>
-          {/* Clerk Sign-in */}
-          <div className="flex items-center">
-              <ClerkUserMenuItem />
-          </div>
+        <div className="flex items-center space-x-8 font-semibold">
+          <Link className="px-3 py-2 hover:text-primary-dark" href="/">
+            Home
+          </Link>
+          <Link className="px-3 py-2 hover:text-primary-dark" href="/quiz">
+            Quiz
+          </Link>
+        </div>
+        <div className="flex items-center">
+          <ClerkUserMenuItem />
+        </div>
       </div>
     </nav>
   );
