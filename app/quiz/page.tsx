a8086d8db829fc2f782e137382a2c623fd7af71c
Ben Briones <benbriones50@gmail.com>
2024-05-08 17:50:34 -0700

    implemented protection to quiz route, check if query param exists, is a uuid, and if property and offer exist in database

--- app/quiz/page.tsx
+++ app/quiz/page.tsx
@@ -1,22 +1,41 @@
 import TypeformWidget from "@/components/TypeformWidget";
 import { redirect } from "next/navigation";
 import { PrismaClient } from "@prisma/client";
+import { validate as isUuid } from "uuid";
 
 const prisma = new PrismaClient();
 
 /** Quiz:  Component for rendering the Quiz page.
  *
- * Quiz -> FormWidget
+ * Quiz -> TypeformWidget
  */
 
 export default async function Quiz({
   searchParams,
 }: {
-  searchParams: { propertyId: string | null; offerId: string | null };
+  searchParams: { propertyId: string | undefined; offerId: string | undefined };
 }) {
   const { propertyId, offerId } = searchParams;
 
-  if (!propertyId || !offerId) redirect("/");
+  // check for undefined query params or invalid UUID format
+  if (!propertyId || !offerId || !isUuid(propertyId) || !isUuid(offerId))
+    redirect("/address-validate");
+
+  // check if property or offer exists, return null if not
+  const property = await prisma.property.findFirst({
+    where: {
+      id: propertyId,
+    },
+  });
+
+  const offer = await prisma.offer.findFirst({
+    where: {
+      id: offerId,
+    },
+  });
+
+  // if no property or offer found, redirect to address-validate
+  if (!property || !offer) redirect("/address-validate");
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
