0e5e71f43819d122d985541c8abc7d80035f356c
Cooper Spinelli <cooperspinelli@gmail.com>
2024-05-09 12:51:15 -0600

    Minor refactor

--- lib/docusign/Envelope.ts
+++ lib/docusign/Envelope.ts
@@ -112,7 +112,7 @@ export default class Envelope {
     const requestData = {
       docGenFormFields: [
         {
-          documentId: documentId,
+          documentId,
           docGenFormFieldList,
         },
       ],
