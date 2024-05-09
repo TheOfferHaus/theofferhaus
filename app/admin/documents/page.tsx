8a0ee8292f7ffd7ea2f2b095d4aec859029d1fe0
Gracee Gallivan <gracee.gallivan@gmail.com>
2024-05-09 16:43:28 -0700

    changed to be a client component so that page re-renders when user uplaods a new file

--- app/admin/documents/page.tsx
+++ app/admin/documents/page.tsx
@@ -1,14 +1,36 @@
+"use client";
+
 import UploadButtonArea from "@/components/UploadThing/UploadButtonArea";
-import {UploadthingApi} from "@/utils/uploadthingApi";
+import { UploadthingApi } from "@/utils/uploadthingApi";
 import { FileTableColumns } from "@/components/UploadThing/FileTable/FileTableColumns";
 import { FileTable } from "@/components/UploadThing/FileTable/FileTable";
+import { File } from "@/utils/uploadthingApi";
+import { useState, useEffect } from "react";
 
-export default async function Documents() {
-  const files = await UploadthingApi.listFiles();
+export default function Documents() {
+  const [files, setFiles] = useState<File[]>([]);
+  const [isLoading, setIsLoading] = useState<Boolean>(true);
+
+  useEffect(() => {
+    async function fetchAllFiles() {
+      const response = await fetch("/api/uploadthing/getfiles");
+      const allFiles = await response.json();
+      setFiles(allFiles);
+      setIsLoading(false);
+    }
+
+    fetchAllFiles();
+  }, []);
+
+  function updateFiles(file: File): void {
+    setFiles((curr) => [...curr, file]);
+  }
+
+  if (isLoading) return <div>Loading...</div>;
 
   return (
     <div className="h-screen mx-32">
-      <UploadButtonArea />
+      <UploadButtonArea updateFileState={updateFiles} />
       <FileTable columns={FileTableColumns} data={files} />
     </div>
   );
