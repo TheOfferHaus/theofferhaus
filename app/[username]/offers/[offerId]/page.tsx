df15d137f809d7f575572aebc248b6a5d8e616e7
Ben Briones <benbriones50@gmail.com>
2024-05-13 15:39:32 -0700

    testing offer detail timers

--- app/[username]/offers/[offerId]/page.tsx
+++ app/[username]/offers/[offerId]/page.tsx
@@ -7,48 +7,68 @@ const prisma = new PrismaClient();
 
 /** Component for offer detail, includes offer/property info from typeform fields and links to offer documents*/
 
-export default async function Offer({ params }: { params: { offerId: string; }; }) {
-    const currUser = await currentUser() as User;
+export default async function Offer({
+  params,
+}: {
+  params: { offerId: string };
+}) {
+  const currUser = (await currentUser()) as User;
 
-    const { offerId } = params;
+  const { offerId } = params;
 
-    const offer = await prisma.offer.findUnique({
-        where: {
-            id: offerId
-        }
-    });
+  const offer = await prisma.offer.findUnique({
+    where: {
+      id: offerId,
+    },
+    include: {
+      property: true,
+    },
+  });
 
-    if (!offer) redirect(`${currUser.username}/offers`);
+  if (!offer) redirect(`${currUser.username}/offers`);
 
-    const { typeformId, envelopeId } = offer;
+  const { typeformId, envelopeId } = offer;
 
-    // const typeformData = fetch(typeformAPI)
-    // const envelope = fetch(docusignAPI)
+  // const typeformData = fetch(typeformAPI) get responses
 
-    const currentDate = new Date();
+  const currentDate = new Date();
 
-    return (
-        <div>
-            <h1>Specific offer page. Cannot see this if not logged in!</h1>
-            <div className="Address">
-                <h2>Property Address</h2>
-            </div>
-            <div className="ClosingDueDate">
-                <h5>Closing Date: </h5>
-                <CountdownTimer targetDate={new Date(currentDate.getTime() + 30 * 24 * 60 * 60 * 1000)} />
-            </div>
-            <div className="NegotiationDueDate">
-                <h5>Negotiation Due Date: </h5>
-                <CountdownTimer targetDate={new Date(currentDate.getTime() + 1 * 60000)} />
-            </div>
-            <div className="AppraisalDueDate">
-                <h5>Appraisal Due Date: </h5>
-                <CountdownTimer targetDate={new Date(currentDate.getTime() + 1 * 60000)} />
-            </div>
-            <div className="InspectionDueDate">
-                <h5>Inspection Due Date: </h5>
-                <CountdownTimer targetDate={new Date(currentDate.getTime() + 1 * 60000)} />
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className="Address">
+        <h1 className="lg:text-5xl md:text-3xl sm:text-3xl text-2xl mb-3 mt-4">
+          Property Address: {offer.property.address}
+        </h1>
+      </div>
+      <div className="ClosingDueDate">
+        <h5>Closing Date: </h5>
+        <CountdownTimer
+          targetDate={
+            new Date('5/14/2024')
+          }
+        />
+      </div>
+      <div className="NegotiationDueDate">
+        <h5>Negotiation Due Date: </h5>
+        <CountdownTimer
+          targetDate={new Date('5/15/2024')}
+        //   targetDate={new Date(currentDate.getTime() + 1 * 60000)}
+        />
+      </div>
+      <div className="AppraisalDueDate">
+        <h5>Appraisal Due Date: </h5>
+        <CountdownTimer
+          targetDate={new Date('5/16/2024')}
+        //   targetDate={new Date(currentDate.getTime() + 1 * 60000)}
+        />
+      </div>
+      <div className="InspectionDueDate">
+        <h5>Inspection Due Date: </h5>
+        <CountdownTimer
+          targetDate={new Date('5/17/2024')}
+        //   targetDate={new Date(currentDate.getTime() + 1 * 60000)}
+        />
+      </div>
+    </div>
+  );
 }
