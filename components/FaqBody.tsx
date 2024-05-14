28910316ab03cadd6436d30ac06ad31295f4839f
David Sapiro <dsap48@gmail.com>
2024-05-14 11:36:51 -0700

    Added nav section functionality with links to questions

--- components/FaqBody.tsx
+++ components/FaqBody.tsx
@@ -1,15 +1,27 @@
 import { FaqData } from "@/lib/faqs/data";
 import FaqCard from "./FaqCard";
 
+/** Body component for the FAQ page */
+
 export default function FaqBody() {
   return (
-    <div className="grid grid-cols-4 my-12 mx-24">
-      <div className="col-span-1">
-        <h1>Placeholder Nav Section </h1>
+    <div className="grid grid-cols-1 md:grid-cols-4 my-12">
+      <div className="md:col-span-1 mb-10 mx-8 md:mr-4 md:ml-12">
+        <p className="font-extrabold text-lg mb-1">Go to</p>
+        <hr className="mb-4" />
+        {FaqData.map((f, i) => (
+          <a href={`#${i}`}>
+            <div className="hover:underline mb-3">
+              {f.question}
+            </div>
+          </a>
+        ))}
       </div>
-      <div className="col-span-3">
-        {FaqData.map((f) => (
-          <FaqCard faq={f} />
+      <div className="md:col-span-3 md:mx-12 mx-8">
+        {FaqData.map((f, i) => (
+          <div id={String(i)}>
+            <FaqCard faq={f} />
+          </div>
         ))}
       </div>
     </div>
