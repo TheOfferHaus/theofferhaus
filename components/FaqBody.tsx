4ac2bb8597a618ed3af8f39f68bf5c1fa0b379a7
Gracee Gallivan <gracee.gallivan@gmail.com>
2024-05-14 13:45:22 -0700

    changed id on FaqCard div from string casting to string interpolation

--- components/FaqBody.tsx
+++ components/FaqBody.tsx
@@ -1,4 +1,4 @@
-import { FaqData } from "@/lib/faqs/data";
+import { FAQs } from "@/lib/faqs/data";
 import FaqCard from "./FaqCard";
 
 /** Body component for the FAQ page */
@@ -9,7 +9,7 @@ export default function FaqBody() {
       <div className="md:col-span-1 mb-10 mx-8 md:mr-4 md:ml-12">
         <p className="font-extrabold text-lg mb-1">Go to</p>
         <hr className="mb-4" />
-        {FaqData.map((f, i) => (
+        {FAQs.map((f, i) => (
           <a href={`#${i}`}>
             <div className="hover:underline mb-3">
               {f.question}
@@ -18,8 +18,8 @@ export default function FaqBody() {
         ))}
       </div>
       <div className="md:col-span-3 md:mx-12 mx-8">
-        {FaqData.map((f, i) => (
-          <div id={String(i)}>
+        {FAQs.map((f, i) => (
+          <div id={`${i}`}>
             <FaqCard faq={f} />
           </div>
         ))}
