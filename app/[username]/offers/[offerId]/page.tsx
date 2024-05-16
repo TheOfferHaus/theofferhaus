ff959b7e6d8c924be1b37330a4b817f563b3a66a
Gracee Gallivan <gracee.gallivan@gmail.com>
2024-05-15 21:28:29 -0700

    Updated styling to prevent bleeding between label and values

--- app/[username]/offers/[offerId]/page.tsx
+++ app/[username]/offers/[offerId]/page.tsx
@@ -31,15 +31,15 @@ export default async function Offer({
   const { typeformId, envelopeId, envelopeURL } = offer;
 
   return (
-    <div className="max-w-screen-lg mx-auto text-center">
+    <div className="mx-auto text-center">
       <div className="p-12 md:px-24 mb-12">
-        <div className="Address text-center sm:mb-20 mb-6">
-          <h1 className="sm:text-3xl text-2xl mb-3 font-extrabold">
+        <div className="Address text-center md:mb-20 mb-12">
+          <h1 className="md:text-5xl sm:text-4xl text-3xl">
             {offer.property.address}
           </h1>
         </div>
 
-        <div className="Map md:mb-24 mb-8 mx-auto relative md:h-[20rem] h-[20rem]">
+        <div className="Map md:mb-24 mb-12 mx-auto relative md:h-[20rem] h-[20rem]">
           <Image
             src="/example-map.jpg"
             alt="PropertyMap"
@@ -48,44 +48,68 @@ export default async function Offer({
           />
         </div>
 
-        <div className="grid grid-cols-1 md:gap-12 md:grid-cols-2">
-          <div className="DueDates mb-16 md:mb-0 text-center sm:text-xl text:lg">
-            <div className="NegotiationDueDate mb-3">
-              <span>
-                Negotiation Due Date: &nbsp;
-                <CountdownTimer targetDate={new Date("5/15/2024")} />
-              </span>
+        <div className="flex justify-center">
+          <div className="grid grid-cols-1 md:gap-12 md:grid-cols-2 sm:text-md text-sm">
+            <div className="DueDates mb-12 md:mb-0 text-left">
+              <h3 className="NegotiationDueDate mb-4">
+                <span className="sm:text-2xl text-xl">
+                  Negotiation Due Date:
+                </span>
+                &nbsp;
+                <span className="italic">
+                  <CountdownTimer targetDate={new Date("5/15/2024")} />
+                </span>
+              </h3>
+              <h3 className="AppraisalDueDate mb-4">
+                <span className="sm:text-2xl text-xl">Appraisal Due Date:</span>{" "}
+                &nbsp;
+                <span className="italic">
+                  <CountdownTimer targetDate={new Date("5/16/2024")} />
+                </span>
+              </h3>
+              <h3 className="InspectionDueDate">
+                <span className="sm:text-2xl text-xl">
+                  Inspection Due Date:
+                </span>{" "}
+                &nbsp;
+                <span className="italic">
+                  <CountdownTimer targetDate={new Date("5/17/2024")} />
+                </span>
+              </h3>
             </div>
-            <div className="AppraisalDueDate mb-3">
-              <span>
-                Appraisal Due Date: &nbsp;
-                <CountdownTimer targetDate={new Date("5/16/2024")} />
-              </span>
-            </div>
-            <div className="InspectionDueDate mb-3">
-              <span>
-                Inspection Due Date: &nbsp;
-                <CountdownTimer targetDate={new Date("5/17/2024")} />
-              </span>
-            </div>
-          </div>
-          <div className="OfferInfo text-center sm:text-xl text-lg">
-            <h2 className="mb-3">Current Offer Amount: {`$${offer.price}`}</h2>
-            <div className="ClosingDueDate mb-3">
-              <span>
-                Closing Date: &nbsp;
-                <CountdownTimer targetDate={new Date("5/14/2024")} />
-              </span>
-              <h2 className="mb-3">Down Payment: {`$150000`}</h2>
-              <h2 className="mb-3">Earnest Money: {`$50000`}</h2>
+            <div className="OfferInfo text-left md:mx-auto mx-0">
+              <h3 className="mb-4">
+                <span className="sm:text-2xl text-xl">
+                  Current Offer Amount:
+                </span>{" "}
+                &nbsp;
+                <span className="italic">{`$${offer.price}`}</span>
+              </h3>
+              <h3 className="ClosingDueDate mb-4">
+                <span className="sm:text-2xl text-xl">Closing Date:</span>{" "}
+                &nbsp;
+                <span className="italic">
+                  <CountdownTimer targetDate={new Date("5/14/2024")} />
+                </span>
+              </h3>
+              <h3 className="mb-4">
+                <span className="sm:text-2xl text-xl">Down Payment:</span>{" "}
+                &nbsp;
+                <span className="italic">{`$150000`}</span>
+              </h3>
+              <h3>
+                <span className="sm:text-2xl text-xl">Earnest Money:</span>{" "}
+                &nbsp;
+                <span className="italic">{`$50000`}</span>
+              </h3>
             </div>
           </div>
         </div>
 
         <div className="flex justify-center my-12">
           <button
-            className="mt-5 px-10 py-6 text-xl bg-custom-white text-black font-semibold rounded-lg shadow-md
-              hover:bg-black hover:text-white focus:outline-none focus:ring-2
+            className="md:mt-5 px-10 py-6 text-xl bg-custom-white text-black font-semibold rounded-lg shadow-md
+              hover:bg-black hover:text-white focus:outline-none
               focus:ring-light-gray focus:ring-opacity-75 animation: : transition
               duration-150 ease-in-out transform hover:scale-95"
           >
