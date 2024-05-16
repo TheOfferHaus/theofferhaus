5f1c409762a77e0ddcbd09844c24b3d5a54c81be
Cooper Spinelli <cooperspinelli@gmail.com>
2024-05-15 18:54:32 -0600

    Add useEffect for displaying toast message on mount

--- components/ValidateRedirectAlert.tsx
+++ components/ValidateRedirectAlert.tsx
@@ -1,19 +1,17 @@
 "use client";
 
-import { Toaster, toast } from "sonner";
+import { toast } from "sonner";
 import { useEffect } from "react";
 
 /** Toast alert message on redirect. */
 
 const ValidateRedirectAlert = () => {
-  toast("My first toast");
 
-  return (
-    <div>
-      <Toaster />
-      <button onClick={() => toast("My first toast")}>Give me a toast</button>
-    </div>
-  );
+  useEffect(() => {
+    toast("You currently have an application in progress!");
+  }, []);
+
+  return <></>;
 };
 
 export default ValidateRedirectAlert;
